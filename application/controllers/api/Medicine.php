bf8c4f0ee43b3e10dc7b92ee3f8f851c8dcf8afd
shabarhat <aroob4shoaib131@gmail.com>
2020-12-07 00:03:54 +0530

    Controller files are modified

---
+++ application/controllers/api/Medicine.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Medicine extends CI_Controller {
+    public function medicine()
+    {
+
+    }
+}
+?>
---
+++ application/controllers/api/Patient_visit.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Patient_visit extends CI_Controller {
+    public function patient_visit()
+    {
+
+    }
+}
+?>
